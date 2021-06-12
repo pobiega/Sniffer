f1fc9ee83c588e012f52100d7055adb1ff5d381e
Pobiega <robin.aakerman@gmail.com>
2021-06-12 12:47:03 +0200

    placeholder code for an ESI client

---
+++ src/Sniffer/Data/ESIClient.cs
@@ -0,0 +1,27 @@
+﻿using Sniffer.Data.ESI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sniffer.Data
+{
+    public class ESIClient : CacheBase
+    {
+        private const string ESI_BASE_URL = "https://esi.evetech.net/latest";
+
+        public async Task<SystemData> GetSystemDataAsync(int systemId)
+        {
+            return await CachedHttpGetAsync<SystemData>($"{ESI_BASE_URL}/universe/systems/{systemId}/?datasource=tranquility&language=en", 180);
+        }
+    }
+
+    public abstract class CacheBase
+    {
+        public async Task<T> CachedHttpGetAsync<T>(string url, int cacheTime)
+        {
+            return await Task.FromResult(default(T));
+        }
+    }
+}
