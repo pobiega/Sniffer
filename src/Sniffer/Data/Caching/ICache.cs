145aad1f6f5261ac5bed98c4e2db42680fae0ae1
Pobiega <robin.aakerman@gmail.com>
2021-06-20 16:47:27 +0200

    add benchmarking and persistance, and expand on the ESI client prototype

---
+++ src/Sniffer/Data/Caching/ICache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sniffer.Data.Caching
+{
+    public interface ICache
+    {
+        Task<T> GetOrCreate<T>(string key, Func<Task<T>> factory);
+    }
+}
